a585aca122aaad24eb268cfad5b9536cea6a5ff9
d5-bit <d5@choup.es.kr>
2025-08-02 09:36:00 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,21 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        basic.showIcon(IconNames.Heart)
+    }
+    if (receivedNumber == 2) {
+        basic.showIcon(IconNames.Yes)
+    }
+    if (receivedNumber == 3) {
+        basic.showIcon(IconNames.Happy)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(4)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(6)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(5)
+})
+radio.setGroup(3)
